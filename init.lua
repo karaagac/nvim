a256eb4113b609bf9e11e1302f946185e1ea1810
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-05-28 15:55:30 -0400

    new files added

---
+++ init.lua
@@ -0,0 +1,20 @@
+-- https://www.youtube.com/watch?v=4zyZ3sw_ulc&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&index=2
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts={}
+
+require("vim-options")
+require("lazy").setup("plugins")
+
