799fc6426f669eaae2c369b9abdef301ac34a923
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-05-28 19:02:22 -0400

    new lines added to quiting and writing

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,13 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader=" "
+
+-- Map the quit without saving command to <Leader>q
+vim.cmd('nnoremap <leader>q <Cmd>q!<CR>')
+
+-- Map the kill current buffer command to <Leader>k
+vim.cmd('nnoremap <leader>k <Cmd>bd<CR>')
+
+-- Map the write command to <Leader>w
+vim.cmd('nnoremap <leader>w <Cmd>w<CR>')
+
