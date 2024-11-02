667145e79b3776829445613641d027cfe2879023
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-11-01 23:16:39 -0400

    running java class in neovim keybinding added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,7 +7,8 @@ vim.cmd("set shiftwidth=2")
 -- Enable absolute line numbers
 vim.cmd("set number")              -- Show absolute line numbers
 
-vim.g.mapleader=" "
+-- Set leader key
+vim.g.mapleader = " "
 
 -- Map the quit without saving command to <Leader>q
 vim.cmd('nnoremap <leader>q <Cmd>q!<CR>')
@@ -18,4 +19,5 @@ vim.cmd('nnoremap <leader>k <Cmd>bd<CR>')
 -- Map the write command to <Leader>w
 vim.cmd('nnoremap <leader>w <Cmd>w<CR>')
 
-
+-- Map <Leader>r to compile and run the current Java file
+vim.cmd('nnoremap <leader>r <Cmd>!javac % && java %:r<CR>')
