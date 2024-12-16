6639016d36e015de702cf003237452a9bf51bf1d
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-12-16 14:02:17 -0500

    neotree open/close updated

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,7 @@ return {
       "MunifTanjim/nui.nvim",  
   },
 config = function()
-  vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+  -- vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', {})
 end
 } 
